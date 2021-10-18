4d6d86dd22c8a2ce7fece246b8fd8417acbf0b21
shariful islam <sharifulb07@gmail.com>
2021-10-18 21:23:05 +0600

    This contains different tutorials of various topics of HTML

---
+++ GoogleMap/index.js
@@ -0,0 +1,7 @@
+let map;
+function initMap(){
+    map=new google.maps.map(document.getElementById("map"),{
+        center:{lat:-34.397, lng: 150.644 },
+        zoom:8,
+    });
+        }
---
+++ HTML javascript/external.js
@@ -0,0 +1,5 @@
+function fun() {  
+   var x = document.getElementById("frm1").value;
+    // var x = document.getElementById("frm1").value;  
+     alert("Hi"+" "+x+" "+ "you have successfully submitted the details");  
+ }  
